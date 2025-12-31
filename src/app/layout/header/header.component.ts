a3682981c6fbc84055bfbec5f949276409caf480
Huicong Huang <huicong.huang21@gmail.com>
2025-12-31 17:22:34 +0800

    Add ngTemplate for header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface menuInfo{
+  name:string,
+  list?:menuInfo[],
+  path:string
+}
+
 @Component({
   selector: 'app-header',
   standalone: false,
@@ -7,5 +13,75 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  menuList:menuInfo[] = [
+    {
+      name:'首页 ',
+      path:''
+    },
+    {
+      name:'全部课程 ',
+      path:'',
+    },
+    {
+      name:'Unity ',
+      path:'',
+      list:[
+        {
+          name:'Unity 全部课程',
+          path:'/class'
+        },
+        {
+          name:'Unity A计划（永久）',
+          path:'/plan/2/introduction'
+        },
+        {
+          name:'Unity学习路线',
+          path:''
+        },
+      ]
+    },
+    {
+      name:'虚幻 ',
+      path:''
+    },
+    {
+      name:'Cocos ',
+      path:''
+    },
+    {
+      name:'Java ',
+      path:''
+    },
+    {
+      name:'Python AI ',
+      path:''
+    },
+    {
+      name:'小程序',
+      path:'',
+      list:[
+        {
+          name:'微信小程序 全部课程',
+          path:'/class'
+        },
+        {
+          name:'微信小程序 A计划（永久）',
+          path:'/plan/1/introduction'
+        },
+        {
+          name:'微信小程序学习路线',
+          path:''
+        },
+      ]
+    },
+    {
+      name:'合作课程',
+      path:''
+    },
+    {
+      name:'其他',
+      path:''
+    },
+  ]
 
 }
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,17 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
-
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 
 @NgModule({
-  declarations: [
-    LayoutComponent,
-    HeaderComponent,
-    FooterComponent
-  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule, NzLayoutModule, NzDropDownModule
+  ],
+  declarations: [LayoutComponent,HeaderComponent,FooterComponent],
+  exports: [LayoutComponent]
 })
 export class LayoutModule { }
