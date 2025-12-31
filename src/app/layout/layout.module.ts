4d7b298c0252d0b486420043e7d913186599d926
Huicong Huang <huicong.huang21@gmail.com>
2025-12-31 17:51:50 +0800

    Add icons for dropdown

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,10 +5,11 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
   imports: [
-    CommonModule, NzLayoutModule, NzDropDownModule
+    CommonModule, NzLayoutModule, NzDropDownModule, NzIconModule
   ],
   declarations: [LayoutComponent,HeaderComponent,FooterComponent],
   exports: [LayoutComponent]
