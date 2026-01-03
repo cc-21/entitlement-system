0a08c242c0e138deb8bd5923a684fa28384068eb
Huicong Huang <huicong.huang21@gmail.com>
2026-01-03 18:59:44 +0800

    Dropdown for changing themes

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -6,10 +6,11 @@ import { FooterComponent } from './footer/footer.component';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 
 @NgModule({
   imports: [
-    CommonModule, NzLayoutModule, NzDropDownModule, NzIconModule
+    CommonModule, NzLayoutModule, NzDropDownModule, NzIconModule, NzAvatarModule
   ],
   declarations: [LayoutComponent,HeaderComponent,FooterComponent],
   exports: [LayoutComponent]
