0caf71e9043a3cddfe8136f9f08703db66a66a7e
Huicong Huang <huicong.huang21@gmail.com>
2026-01-05 11:47:45 +0800

    分段选择器

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { NzCarouselModule } from 'ng-zorro-antd/carousel';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzSegmentedModule } from 'ng-zorro-antd/segmented';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 @Component({
   selector: 'app-home',
-  imports: [NzCarouselModule],
+  imports: [NzCarouselModule, NzButtonModule,NzSegmentedModule,NzGridModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.less'
 })
@@ -31,5 +34,92 @@ export class HomeComponent {
     {
       src:'../../assets/images/carousel/7.jpg'
     },
-  ]
+  ];
+
+  courseList:any[] = [
+    {
+      type:'全部课程',
+      children:[
+        {
+          name:`Unity最全最完整的零基础入门教程-Luna's Fantasy（露娜的大冒险）`,
+          url:'https://www.sikiedu.com/files/course/2023/08-02/142256046fe1999314.jpg',
+          count:8454,
+          comments:66,
+          isFree:true
+        },
+        {
+          name:`C#编程-第一季-编程基础-宇宙最简单2021最新版`,
+          url:'https://www.sikiedu.com/files/course/2021/10-14/17192914e765627633.png',
+          count:28017,
+          comments:78,
+          isFree:true
+        },
+        {
+          name:`Unity版植物大战僵尸：游戏制作完全指南`,
+          url:'https://www.sikiedu.com/files/course/2023/12-22/1038113a58c4010564.png',
+          count:5652,
+          comments:4,
+          isFree:true
+        },
+        {
+          name:`《土豆英雄》使用Unity完整复刻开发-高还原度-第一季`,
+          url:'https://www.sikiedu.com/files/course/2024/10-31/10375866fccf393026.jpg',
+          count:438,
+          comments:2,
+          isFree:true
+        }
+      ]
+    },
+    {
+      type:'Unity',
+    },
+    {
+      type:'虚幻',
+    },
+    {
+      type:'Cocos',
+    },
+    {
+      type:'独立游戏',
+    },
+    {
+      type:'Java',
+    },
+    {
+      type:'Python人工智能',
+    },
+    {
+      type:'游戏美术',
+    },
+    {
+      type:'前端',
+    },
+    {
+      type:'微信小程序',
+      children:[
+        {
+          name:`快速上手微信小程序UI框架`,
+          url:'https://www.sikiedu.com/files/course/2020/09-02/132725d23094370998.png',
+          count:160,
+          comments:2,
+          isFree:false,
+          price:240
+        }
+      ]
+    },
+    {
+      type:'Linux运维',
+    },
+    {
+      type:'单卖课',
+    },
+    {
+      type:'大数据',
+    },
+    {
+      type:'其他',
+    },
+  ];
+  selectIndex:number = 0;
+  options=['最新', '最热', '推荐']
 }
