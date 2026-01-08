1375009af1198f69ebc492f52392f09710ec8fd2
Huicong Huang <huicong.huang21@gmail.com>
2026-01-08 13:27:00 +0800

    Use count pipe for price

--- src/app/component/card/card.component.ts
+++ src/app/component/card/card.component.ts
@@ -3,10 +3,11 @@ import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzFlexModule } from 'ng-zorro-antd/flex';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { CountPipe } from '../pipe/count.pipe';
 
 @Component({
   selector: 'app-card',
-  imports: [NzCardModule,NzFlexModule,NzIconModule,NzGridModule],
+  imports: [NzCardModule,NzFlexModule,NzIconModule,NzGridModule,CountPipe],
   templateUrl: './card.component.html',
   styleUrl: './card.component.less'
 })
